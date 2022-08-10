3cba5d882f838305adc79ee41ccf6f6208a76af6
CHANGHEE HAN <79398161+Ch4n9hee@users.noreply.github.com>
2022-08-10 10:49:22 +0900

    블록체인 완성

--- build/index.js
+++ build/index.js
@@ -1,11 +1,2 @@
 "use strict";
-const hello = () => 'hi';
-class Block {
-    constructor(data) {
-        this.data = data;
-    }
-    static hello() {
-        return 'hi';
-    }
-}
-const a = 'abc';
+console.log('bye');
